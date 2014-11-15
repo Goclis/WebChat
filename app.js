d261043d4f4b599b101f0ad6d6ae0aa4a1936426
Goclis Yao <goclisyyh@gmail.com>
2014-11-15 20:09:55 +0800

    Remove some codes for discarded features

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ var io = require('socket.io');
 var fs = require('fs');
 
 var server = http.createServer(function (req, res) {
-    fs.readFile('views/index.html', { encoding: 'utf-8' }, 
-            function (err, data) {
+    fs.readFile('views/index.html', { encoding: 'utf-8' }, function (err, data) {
         if (err) {
             res.end('Error');
         } else {
@@ -17,14 +16,16 @@ var server = http.createServer(function (req, res) {
 var server_socket = io.listen(server);
 server.listen(3000);
 
+
 var client_list = [];
+var nickname_list = ['A', 'B', 'C', 'D'];
 server_socket.on('connection', function (client_socket) {
-    client_socket.id_number = new Date().getTime();  
+    var timestamp = new Date().getTime();
+    client_socket.nickname = nickname_list[timestamp % 4] + ' ' + Math.round(timestamp / 1000000);
     client_list.push(client_socket);
 
     client_socket.on('my_msg', function (msg, callback) {
         broadcast(msg, client_socket);
-
         callback('ok');
     });
 
@@ -38,7 +39,7 @@ function broadcast(msg, client_socket) {
     for (var i = 0; i < client_list.length; ++i) {
         if (client_list[i] !== client_socket) {
             client_list[i].emit('others_msg', {
-                nickname: client_socket.id_number,
+                nickname: client_socket.nickname,
                 msg: msg
             });
         }
