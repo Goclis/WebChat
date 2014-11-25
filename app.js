60c1e106cb2a843aecfafbbed8b80160ce72490c
Goclis Yao <goclisyyh@gmail.com>
2014-11-25 09:52:52 +0800

    Add some static route

--- app.js
+++ app.js
@@ -1,17 +1,60 @@
 var http = require('http');
 var io = require('socket.io');
 var fs = require('fs');
+var url = require('url');
 
 var server = http.createServer(function (req, res) {
-    fs.readFile('views/index.html', { encoding: 'utf-8' }, function (err, data) {
-        if (err) {
-            res.end('Error');
-        } else {
-            res.writeHead(200, {'content-type': 'text/html'});
-            res.end(data, 'utf-8');
-        }
-    });
+    var pathname = url.parse(req.url).pathname;
+
+    console.log('Request for ' + pathname + ' received.');
+
+    route(pathname, req, res);
 });
+
+function route (pathname, req, res) {
+    if (pathname === '/static/jquery.mobile-1.3.2.min.css') {
+        fs.readFile('statics/jquery.mobile-1.3.2.min.css', 
+                { encoding: 'utf-8' }, function (err, data) {
+            if (err) {
+                res.end('Error');
+            } else {
+                res.writeHead(200, {'content-type': 'text/css'});
+                res.end(data, 'utf-8');
+            }
+        });
+    } else if (pathname === '/static/jquery.mobile-1.3.2.min.js') {
+        fs.readFile('statics/jquery.mobile-1.3.2.min.js', 
+                { encoding: 'utf-8' }, function (err, data) {
+            if (err) {
+                res.end('Error');
+            } else {
+                res.writeHead(200, {'content-type': 'application/x-javascript'});
+                res.end(data, 'utf-8');
+            }
+        });
+    } else if (pathname === '/static/jquery-1.8.3.min.js') {
+        fs.readFile('statics/jquery-1.8.3.min.js', 
+                { encoding: 'utf-8' }, function (err, data) {
+            if (err) {
+                res.end('Error');
+            } else {
+                res.writeHead(200, {'content-type': 'application/x-javascript'});
+                res.end(data, 'utf-8');
+            }
+        });
+    } else if (pathname === '/favicon.icon') {
+        res.end('No icon');
+    } else {
+        fs.readFile('views/index.html', { encoding: 'utf-8' }, function (err, data) {
+            if (err) {
+                res.end('Error');
+            } else {
+                res.writeHead(200, {'content-type': 'text/html'});
+                res.end(data, 'utf-8');
+            }
+        });
+    }
+}
 
 var server_socket = io.listen(server);
 server.listen(3000);
